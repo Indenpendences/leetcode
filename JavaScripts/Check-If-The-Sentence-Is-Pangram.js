2a6aa3aa18bbf95d6bf248107761c685d4c4e588
nguyenkhanh17112003@gmail.com <nguyenkhanh17112003@gmail.com>
2022-10-17 17:23:42 +0700

    add ease

---
+++ JavaScripts/Check-If-The-Sentence-Is-Pangram.js
@@ -0,0 +1,12 @@
+var checkIfPangram = function (sentence) {
+  let alpha = sentence.toUpperCase().split("");
+  for (let beta = 65; beta < 91; beta++) {
+    let gamma = String.fromCharCode(beta);
+    if (alpha.includes(gamma)) {
+      continue;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
